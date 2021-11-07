12ab4b36907da807b3832c0d4b6b28585c211717
William Wei <weiwilli@usc.com>
2021-11-06 17:30:04 -0700

    Addition Game Screen

--- src/frontend/GameAddition.js
+++ src/frontend/GameAddition.js
@@ -1,7 +1,75 @@
-function GameAddition() {
-    return (
-      <div>Hi </div>
-    );
-  }
-  
-  export default GameAddition;
+
+import React, { useState } from "react";
+
+export default function GameAddition() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [sum, setSum] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const generateQuestion = () => {
+    setNum1(Math.ceil(Math.random() * 10));
+    setNum2(Math.ceil(Math.random() * 10));
+  };
+
+  const startCommands = () => {
+    generateQuestion();
+    setScore((score) => score - score);
+}
+
+  const submit = (e) => {
+    e.preventDefault();
+    const formValid = +sum >= 0;
+    if (!formValid) {
+      return;
+    }
+    if (+num1 + +num2 === +sum) {
+      setScore((score) => score + 1);
+    }
+    generateQuestion();
+  };
+
+  return (
+    <div className="GameAddition">
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <button type="button" onClick={startCommands}>
+        start game
+      </button>
+      </div>
+      <form onSubmit={submit}>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+          <label>
+            {num1} + {num2}
+          </label>
+        </div>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+          <input value={sum} onChange={(e) => setSum(e.target.value)} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <p>score: {score}</p >
+      </div>  
+    </div>
+  );
+}
+
+
+
+  //export default GameAddition;
