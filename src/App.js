f78d4bdea2e62dd305d7eca7ac11c21ee4245e3c
Unnati Goyal <unnatigoyal900@gmail.com>
2021-11-16 19:15:10 -0800

    Leaderboard Display

--- src/App.js
+++ src/App.js
@@ -7,26 +7,14 @@ import {
   Route,
 } from "react-router-dom";
 
-import Auth from './frontend/Auth'
+ import Auth from './frontend/Auth'
 import Leaderboard from './frontend/Leaderboard'
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to ="/leaderboard">Leaderboard</Link>
-            </li>
-          </ul>
-        </nav>
+        
         {/* 
         
           React Router Dom Guide: https://v5.reactrouter.com/web/guides/quick-start
@@ -36,11 +24,8 @@ function App() {
           <Route path="/login">
             <Auth />
           </Route>
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
           <Route path="/">
-            <p>Trivia Game</p>
+            <Leaderboard />
           </Route>
         </Switch>
       </div>
--- src/frontend/Leaderboard.js
+++ src/frontend/Leaderboard.js
@@ -1,6 +1,36 @@
+import react from "react";
+import "./styling.css"
 function Leaderboard() {
     return (
-      <div>Leaderboard</div>
+    
+	// <div class = "parent">
+// 	<div class = "mid">  <a> Leaderboard </a> 	</div>
+// 					<div class = "tables">
+// 					<ul class = "hello">
+// 		   				 <li> Addition </li>
+// 		    
+// 		    			 <li> Subtraction</li>
+// 		   				  <li> Multiplication </li>
+// 		   				  <li> Division </li>
+// 		   				  </ul>
+// 		    
+// 					</div>
+// 	</div>
+// 					
+
+
+
+<div class = "parent">
+	<div class = "mid">  <a> Leaderboard </a> 	</div>
+	
+	<div class = "addition">  <a> Addition </a> 	</div>
+	<div class = "subtraction">  <a> Subtraction </a> 	</div>
+	<div class = "multiplication">  <a> Multiplication </a> 	</div>
+	<div class = "division">  <a> Division </a> 	</div>
+					
+	</div>
+					
+		
     );
   }
   
