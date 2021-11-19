48cf232b078ccf54407184ddba2725f5869ebae0
jjl418 <jlee3932@usc.edu>
2021-11-18 17:50:45 -0800

    (julianne lee) added music, changed questions to multiple choice, and changed color

--- src/frontend/GameAddition.js
+++ src/frontend/GameAddition.js
@@ -4,28 +4,42 @@ import {Button, TextField, Typography, Grid} from '@mui/material';
 import { blue } from "@mui/material/colors";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CheckIcon from '@mui/icons-material/Check';
+import useSound from 'use-sound';
+import undertale from '../resources/undertale.m4a';
 
 const bg ='#e1bee7';
 const question_number = 1;
-const first_number = 8821;
-const sign = "+";
-const second_number = 1078;
+const sign = "+"; 
 const filler = ' ';
 
 function GameAddition() {
+  const [firstNumber, setFirstNumber] = useState(8821);
+  const [secondNumber, setSecondNumber] = useState(1078);
+
   const [answer, setAnswer] = useState("");
   const [sec, setSec] = React.useState(30);
+  const [play] = useSound(undertale);
   
   useEffect(() => {
-    if(sec < 0)
+    if(sec === 0)
     {
-      setSec(0);
+      //setSec(0);
+      GetNextScreen()
     }
     else if(sec > 0)
     {
       setTimeout(() => setSec(sec - 1), 1000);
     }
   })
+
+  useEffect(()=>play(),[play])
+/*this function will be used to change the questions. Eventually we will change the parameters to call the backend functions*/
+  function GetNextScreen()
+  {
+    setSec(30);
+    setFirstNumber(1234);
+    setSecondNumber(5678);
+  }
 
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center" sx={{height: '100vh', backgroundColor: bg}}>
@@ -43,7 +57,7 @@ function GameAddition() {
         </Typography>
         <Typography align="center" variant="h3">
         <br/>
-            <b>{first_number} {sign} {second_number} = ?</b>
+            <b>{firstNumber} {sign} {secondNumber} = ?</b>
         </Typography>
         <Typography align="center" variant="h3">
         </Typography>
@@ -53,23 +67,66 @@ function GameAddition() {
         <Typography align="center" variant="h3">
         <br/>
         </Typography>
-        <Grid item xs={6} container direction="row">
-        <TextField 
+        <Grid item xs={6} container direction="row" spacing={1}>
+
+        {/* <TextField 
               style = {{
               width: 300
               }} 
-              inputProps={{min: 0, style: { textAlign: 'center' }}} /*centers the "Answer" in textfield*/
+              inputProps={{min: 0, style: { textAlign: 'center' }}}
               placeholder="Answer" 
-              onChange={(e)=>setAnswer(e.target.value)}></TextField>
+              onChange={(e)=>setAnswer(e.target.value)}></TextField> */}
+          <Grid item xs={6}>
+            <Button style={{
+              //margin: '0px 5px 0px 0px',
+              width: '28em',
+              borderRadius: 7,
+              backgroundColor: "#26c6da", 
+              padding: "9px 18px",
+              fontSize: "18px",
+              color: 'black'
+              }} 
+              variant="contained"><b>Option 1</b></Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button style={{
+              //margin: '0px 5px 0px 0px',
+              width: '28em',
+              borderRadius: 7,
+              backgroundColor: "#f48fb1", /*styling "check" button*/
+              padding: "9px 18px",
+              fontSize: "18px",
+              color: 'black'
+              }} 
+              variant="contained"><b>Option 2</b></Button>
+          </Grid>
           
-          
-          <Button style={{
-            borderRadius: 7,
-            backgroundColor: "#26c6da",
-            padding: "9px 18px",
-            fontSize: "18px"
-            }} 
-            variant="contained">Check</Button>
+          <Grid item xs={6}>
+            <Button style={{
+              //margin: '5px 0px 0px 0px',
+              width: '28em',
+              borderRadius: 7,
+              backgroundColor: "#ffb74d", /*styling "check" button*/
+              padding: "9px 18px",
+              fontSize: "18px",
+              color: 'black'
+              }} 
+              variant="contained"><b>Option 3</b></Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button style={{
+              //margin: '5px 0px 0px 5px',
+              width: '28em',
+              borderRadius: 7,
+              backgroundColor: "#81c784", /*styling "check" button*/
+              padding: "9px 18px",
+              fontSize: "18px",
+              color: 'black'
+              }} 
+              variant="contained"><b>Option 4</b></Button>
+            </Grid>
         </Grid>
       </Grid>
     </Grid>
