286d52184ace1a68e7e2b84ce9f1a5a911210dfa
yjaju <yjaju@usc.edu>
2021-12-01 15:25:43 -0800

    springboot connection made

--- src/backend/src/main/java/com/example/demo/TriviaApplication.java
+++ src/backend/src/main/java/com/example/demo/TriviaApplication.java
@@ -1,11 +1,12 @@
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TriviaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TriviaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaApplication.class, args);
+    }
 }
--- src/backend/src/main/java/com/example/demo/controller/UserController.java
+++ src/backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +17,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
