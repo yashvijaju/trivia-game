0fc81abb83e88dac410b15ffd981d309c3549dc4
Unnati Goyal <unnatigoyal900@gmail.com>
2021-12-01 12:53:12 -0800

    branch

--- src/frontend/Leaderboard.js
+++ src/frontend/Leaderboard.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import MediaCard from "./Components/card"
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import "./styling.css"
 
-function Leaderboard() {
-    return (
+// function Leaderboard() {
+//     return (
     
 	// <div class = "parent">
 // 	<div class = "mid">  <a> Leaderboard </a> 	</div>
@@ -20,6 +28,7 @@ function Leaderboard() {
 // 					
 
 
+{/* <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<div class = "parent">
 	<div class = "mid">  <a> Leaderboard </a> 	</div>
@@ -35,4 +44,52 @@ function Leaderboard() {
     );
   }
   
-export default Leaderboard;
+export default Leaderboard; */
+//}
+function Leaderboard() {
+    return (
+      <>
+        <>
+          <Navbar />
+        </>
+        
+        <Grid>
+        <Grid item xs ={12}>
+              <BasicCard/>
+        </Grid>
+        </Grid>
+        <br />
+        <br />
+        <>
+          <Grid container spacing={2}>
+          
+            <Grid item xs ={6} sm={3}>
+              <MediaCard />
+            </Grid>
+            
+              <Grid item xs ={6} sm={3}>
+              <MediaCard1 />
+              
+
+            </Grid>
+
+            <Grid item xs ={6} sm={3}>
+              <MediaCard2 />
+            </Grid>
+
+            <Grid item xs ={6} sm={3}>
+              <MediaCard3 />
+            </Grid>
+
+
+          
+          </Grid>
+        </>
+      
+
+      </>
+    
+    );
+  }
+  
+  export default Home;
